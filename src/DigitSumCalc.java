ac7a850aef24430e9d5a926ed8b5382e7d09e598
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 13:17:16 -0600

    completed challenge

--- src/DigitSumCalc.java
+++ src/DigitSumCalc.java
@@ -2,10 +2,16 @@ public class DigitSumCalc {
     public static int sumDigits(int number){
         if (number < 10){
             return -1;
-        } else{
-            while()
-
         }
-
+        int sum = 0;
+        // 125  -> 125 / 10 = 12 -> 12 * 10 = 120 -> 125 - 120 = 5
+        while(number > 0){
+            // extract least significant digit
+            int digit = number % 10;
+            sum += digit;
+            // drop least significant digit
+            number /= 10 ; // same as number = number / 10;
+        }
+        return sum;
     }
 }
