72fee328e6dd9470c01f138a79679f4cb5ffe037
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 13:06:29 -0600

    working on sumDigits method

---
+++ src/DigitSumCalc.java
@@ -0,0 +1,11 @@
+public class DigitSumCalc {
+    public static int sumDigits(int number){
+        if (number < 10){
+            return -1;
+        } else{
+            while()
+
+        }
+
+    }
+}
